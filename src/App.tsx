32e3a7fcf4bedacd40fba9791383ee78a9d34631
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-07-25 16:47:42 +0600

    [ui,route]: base page w all routes; all component in routes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import Attraction from './components/Attraction';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import BubbleParticle from './components/BubbleParticle';
+import HighlightedBox from './components/HighlightedBox';
+import MeteorParticle from './components/MeteorParticle';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function BasePage() {
+    return (
+        <div>
+            <h1>Component Demos</h1>
+            <ul>
+                <li><Link to="/attraction">Attraction</Link></li>
+                <li><Link to="/bubble-particle">Bubble Particle</Link></li>
+                <li><Link to="/highlighted-box">Highlighted Box</Link></li>
+                <li><Link to="/meteor-particle">Meteor Particle</Link></li>
+            </ul>
+        </div>
+    );
 }
 
-export default App
+const App = () => <Router>
+    <Routes>
+        <Route path="/" element={<BasePage />} />
+        <Route path="/attraction" element={<Attraction />} />
+        <Route path="/bubble-particle" element={<BubbleParticle />} />
+        <Route path="/highlighted-box" element={<HighlightedBox />} />
+        <Route path="/meteor-particle" element={<MeteorParticle />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+</Router>;
+
+export default App;
