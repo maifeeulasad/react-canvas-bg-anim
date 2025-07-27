1e87ab98642a50babcd8cb9604740d04fe851bd4
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-07-28 01:06:49 +0600

    [feat]: add components index file for exports;

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import Attraction from './Attraction';
+import BubbleParticle from './BubbleParticle';
+import HighlightedBox from './HighlightedBox';
+import MeteorParticle from './MeteorParticle';
+
+
+export { Attraction, BubbleParticle, HighlightedBox, MeteorParticle };
